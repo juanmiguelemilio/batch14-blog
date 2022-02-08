9388f03adaa7d197f6f3cae6bf04a098dc969029
MJ Aguirre <michaelaquila1998@gmail.com>
2022-02-08 20:34:33 +0800

    Initial commit

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,22 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get articles_path
+
+    assert_response :success
+  end
+
+  test "should get new" do
+    get articles_new_path
+
+    assert_response :success
+  end
+
+  test "should create article" do
+    post articles_path, params: { article: { name: 'Name', body: 'Body' } }
+
+    assert_response :redirect
+  end
 end
